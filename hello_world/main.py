ff5ee2f7c7e3182b36c2a0e9e91604aa8db0fbbc
Nikita-Deshmukh <89000708+Nikita-Deshmukh@users.noreply.github.com>
2022-01-10 11:25:04 +0530

    Update main.py

--- hello_world/main.py
+++ hello_world/main.py
@@ -25,7 +25,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World! Learning DevOps'
+    return 'Hello DevOps'
 
 
 if __name__ == '__main__':
